7853067090c48edc809d95d91a9782b05a095a01
pettyem <pettyem@whs.school.nz>
2023-03-14 12:50:48 +1300

    Signed-off-by: pettyem <pettyem@whs.school.nz>

---
+++ HelloWorld/Hello.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Hello here. AWSOME
+ *
+ * @author (your name) EMERY!!!!!!
+ * @version (a version number or a date) UNO!
+ */
+public class Hello
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Hello
+     */
+    public Hello()
+    {
+        // initialise instance variables
+        x = 0;
+        System.out.println("hello World");
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+    
+    public void runForever()
+    {
+        while (true);
+    }
+}
---
+++ Integers/IntTest.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class IntTest here.
+ *
+ * emery
+ * 27/02/2023
+ */
+public class IntTest
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    public int year=2023;
+
+    /**
+     * Constructor for objects of class IntTest
+     */
+    public IntTest()
+    {
+        // initialise instance variables
+        x ++;
+        { int y=21;}
+        y ++;
+        System.out.println(x);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
